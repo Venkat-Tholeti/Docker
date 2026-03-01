4b412492cc810ab5b60740424a251ff78eb03277
venkat <devopsaws84s@gmail.com>
2026-03-01 12:13:18 +0530

    EKS

---
+++ EKS/provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.26.0"
+    }
+  }
+}
+
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+#ANY CHANGES IN PROVIDER FILE, WE HAVE TO RUN TERRAFORM INIT -RECONFIGURE
