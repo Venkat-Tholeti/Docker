4d284394543037412a9b17d994b86de8fe57a4d9
Venkat-Tholeti <naidutholeti12@gmail.com>
2026-05-14 15:20:03 +0530

    DockerCreate

--- data.tf
+++ data.tf
@@ -1,20 +1,19 @@
+
 data "aws_ami" "joindevops" {
-  owners           = ["973714476881"]
-  most_recent      = true
+    
+    filter {
+      name = "name"
+      values = ["Redhat-9-DevOps-Practice"]
+    }
+    
+    filter {
+     name   = "root-device-type"
+     values = ["ebs"]
+    }
 
-  filter {
-    name   = "name"
-    values = ["RHEL-9-DevOps-Practice"]
-  }
+    filter {
+     name   = "virtualization-type"
+     values = ["hvm"]
+   }
 
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
 }
-
